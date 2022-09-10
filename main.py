edb2e2440b14fbb5a2f9ae2244cdd0b07670cf8d
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-09-10 18:46:18 +0530

    adds movements type, turn & display logic

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import itertools
 
 from typing import Literal
 
+from exceptions import InvalidGameInputFormat
+
 Grid = list[list[str]]
-Players = Literal[1, 2]
-
-
-class InvalidGameInputFormat(Exception):
-    pass
+Players = Literal['A', 'B']
+Movements = Literal['L', 'R', 'F', 'B']
 
 
 class Game:
@@ -24,23 +23,24 @@ class Game:
         # Player 1 -> P3, P2, P5, P4, P1
         # Player 2 -> P2, P1, P3, P5, P4
 
-        self.__p1_characters = ['B-P2', 'B-P1', 'B-P3',
-                                'B-P5', 'B-P4']  # self.get_characters(player=1)
-        self.__p2_characters = ['A-P3', 'A-P2', 'A-P5',
-                                'A-P4', 'A-P1']  # self.get_characters(player=2)
+        self.__p1_characters = ['P2', 'P1', 'P3',
+                                'P5', 'P4']  # self.get_characters(player=1)
+        self.__p2_characters = ['P3', 'P2', 'P5',
+                                'P4', 'P1']  # self.get_characters(player=2)
 
         self.__grid = self.create_grid()
 
         self.place_characters(
-            player=1, characters=self.__p1_characters
+            player='A', characters=self.__p1_characters
         )
         self.place_characters(
-            player=2, characters=self.__p2_characters
+            player='B', characters=self.__p2_characters
         )
 
-        self.__player_turn: Players = 1
+        self.__player_turn: Players = 'A'
 
-        print(self.__grid)
+        self.display()
+        self.turn()
 
     def get_characters(self, player: Players) -> list[str]:
         """Get the player characters from input as list."""
@@ -66,14 +66,47 @@ class Game:
         for r, c in itertools.product(
             range(Game.GRID_SIZE), range(Game.GRID_SIZE)
         ):
-            if r == Game.GRID_SIZE - 1 and player == 1:
+            if r == Game.GRID_SIZE - 1 and player == 'A':
                 self.__grid[r][c] = f'A-{characters[c]}'
-            elif r == 0 and player == 2:
+            elif r == 0 and player == 'B':
                 self.__grid[r][c] = f'B-{characters[c]}'
         return self.__grid
 
-    def turn(self, ) -> Grid:
+    def move(self, character: str, direction: Movements) -> Grid:
+        """Move the character in a specific direction.
+        Refer `Movements` type for possible values.
+        """
+        return self.__grid
+
+    def display(self) -> None:
+        """Display grid."""
+        for r in self.__grid:
+            for c_val in r:
+                print(c_val, end='\t')
+            print()
+
+    def turn(self) -> Grid:
         print(f"Player {self.__player_turn}'s Move:")
+        movement = input().strip()
+
+        if movement.find(':') == -1:
+            raise InvalidGameInputFormat(
+                "Invalid movement format <character_name>:<move> (e.g:- P1:L, P2:R, P3:F, P3:B)"
+            )
+
+        character, direction = movement.split(':')
+
+        if direction not in {'L', 'R', 'F', 'B'}:
+            raise InvalidGameInputFormat(
+                "Invalid movement format <character_name>:<move> (e.g:- P1:L, P2:R, P3:F, P3:B)"
+            )
+
+        # Move the character & display the grid.
+        self.move(character, direction)
+        self.display()
+
+        # Update the chance.
+        self.__player_turn = 'B' if self.__player_turn == 'A' else 'A'
 
         return self.__grid
 
